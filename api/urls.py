594402cc231bd0c67256d259efedc3bfe40284a4
nagesh.naik@techademy.net <nagesh.naik@techademy.net>
2024-02-28 19:04:57 +0530

    ClassBasedViewCURD

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 
 from django.urls import path, include
 
-from api.views import student_api
+#from api.views import student_api     # Function Based View
+from api.views import StudentAPI
 from home.views import *
 from vege.views import *
 
 urlpatterns = [
-    path('studentapi/', student_api, name="student_api"),
+    #path('studentapi/', student_api, name="student_api"),                              # Function Based view
+    path('studentapi/', StudentAPI.as_view(), name="student_api_classBase"),            # Class Based view
 ]
